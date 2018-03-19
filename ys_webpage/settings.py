ea2a05a6140ea95ce1feae7775730e93d83e9a11
preetydragonov <flsqja12@naver.com>
2018-03-20 00:06:06 +0900

    changed settings

--- ys_webpage/settings.py
+++ ys_webpage/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,14 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     #'django.contrib.staticfiles.finders.DefaultStorageFinder',
 ]
+
+
+AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-ap-northeast-2-285150183432'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
