2074448ecf797fc18e260aa5d4fead0d26cfcb02
preetydragonov <flsqja12@naver.com>
2018-03-19 18:07:13 +0900

    executed migration, collectstatics

--- ys_webpage/settings.py
+++ ys_webpage/settings.py
@@ -123,7 +123,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "searchWord/static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "searchWord/static"),
+    os.path.join(BASE_DIR, "static"),
         ]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
+]
