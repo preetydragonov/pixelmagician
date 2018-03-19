9099294e6cf62dc53a6bacdd32fead8f19da12f6
preetydragonov <flsqja12@naver.com>
2018-03-19 13:16:10 +0900

    modify urls and views. did not add html or css

--- searchWord/views.py
+++ searchWord/views.py
@@ -1,3 +1,11 @@
+from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'searchWord/home.html', {})
+
+
