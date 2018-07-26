e6e1714d3fa09c1f99f2a1012886b7a8fc8b4fce
preetydragonov <flsqja12@naver.com>
2018-07-27 08:45:40 +0900

    300 -> 150

--- EB/searchWord/static/searchWord/js/passWordToS3.js
+++ EB/searchWord/static/searchWord/js/passWordToS3.js
@@ -2,7 +2,7 @@ function passWordToS3(){
 	parsedQueryWord = document.currentScript.getAttribute('queryWordOnScript');
         setTimeout(function(){
                 location.replace("/board/"+parsedQueryWord, '_self');
-        }, 3000);
+        }, 7000);
 }
 
 passWordToS3();
