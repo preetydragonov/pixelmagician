10e507b79a62bb4528ba5597dfbdb36c0396cb73
preetydragonov <flsqja12@naver.com>
2018-08-01 13:09:26 +0900

    parallel url open

--- EB/searchWord/apiRequests.py
+++ EB/searchWord/apiRequests.py
@@ -1,32 +1,73 @@
 import json
-import urllib.request
 from .constants import (APPNAME,
                         HTML,
                         KEY,
                         URL)
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+import asyncio
+
+def internet_resource_getter(post_data):
+    request = Request(URL().ICRAWLER_TRIGGER, post_data)
+    urlopen(request)
 
 def putImagesToS3(data):
-    formattedDataForPuttingOnS3 = formatDataForS3Request(data, requestType="POST")
-    requestForPutImages = urllib.request.Request(URL().ICRAWLER_TRIGGER, formattedDataForPuttingOnS3)
-    response = urllib.request.urlopen(requestForPutImages)
+    postData = []
+    for date_range in createDateRangeList(7):
+        postDatum = formatDataForS3Request(data, date_range, requestType="POST")
+        postData.append(postDatum)
+
+    loop = asyncio.get_event_loop()
+    for postDatum in postData:
+        loop.run_in_executor(None, internet_resource_getter, postDatum)
+
+    # requestsForPutImages = []
+    #
+    # for date_range in createDateRangeList(7):
+    #     request = Request(URL().ICRAWLER_TRIGGER,
+    #                               formatDataForS3Request(data,
+    #                                                      date_range,
+    #                                                      requestType="POST"))
+    #     requestsForPutImages.append(request)
+    #
+    # # requestForPutImages = Request(URL().ICRAWLER_TRIGGER,
+    # #                               formatDataForS3Request(data,
+    # #                                                      date_ranges[0],
+    # #                                                      requestType="POST"))
+    #
+    #
+    #
+    # urlopen(requestForPutImages)
 
 def getImagesFromS3(searchedWord):
-    requestForGetImages = urllib.request.Request(URL().GET_IMAGES_FROM_S3
+    requestForGetImages = Request(URL().GET_IMAGES_FROM_S3
                                               + "?key=" 
                                               + searchedWord 
                                               + ".json")
-    response = urllib.request.urlopen(requestForGetImages).read()
+    response = urlopen(requestForGetImages).read()
     imageUrls = json.loads(response)
     
     return imageUrls
 
-def formatDataForS3Request(data, requestType):
+
+def createDateRangeList(yearRange):
+    dateList = []
+    for i in range(yearRange):
+        year = 2018 - i
+        early = ((year, 1, 1), (year, 6, 30))
+        dateList.append(early)
+        late = ((year, 7, 1), (year, 12, 31))
+        dateList.append(late)
+    return dateList
+
+def formatDataForS3Request(data, date_range, requestType):
     formattedData = {
         "Records": [{
             "s3": {
                 "object": {
                     "body": {
-                        'query_word': data
+                        'query_word': data,
+                        'date_range': date_range
                     }
                 },
                 "s3SchemaVersion": "1.0"
@@ -36,14 +77,12 @@ def formatDataForS3Request(data, requestType):
     }
 
     if(requestType == 'POST'):
-        jsonFormattedData = json.dumps(formattedData)
-        encodedFormattedData = jsonFormattedData.encode('utf-8')
-        return encodedFormattedData
+        return json.dumps(formattedData).encode('utf-8')
 
     elif(requestType == 'GET'):
-        params = urllib.parse.urlencode(formattedData)
+        params = urlencode(formattedData)
         return params
     
     else:
-        return null;
+        return
 
