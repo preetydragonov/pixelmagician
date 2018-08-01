5c71989b485b3f260579ec2fe2f04e3a567d6cb3
preetydragonov <flsqja12@naver.com>
2018-08-01 23:06:48 +0900

    15000 => 0

--- EB/searchWord/static/searchWord/js/goTo.js
+++ EB/searchWord/static/searchWord/js/goTo.js
@@ -1,8 +1,6 @@
 function goTo(){
 	const textValue = document.getElementById("link_id").value;
         const encoded_textValue = encodeURIComponent(textValue);
-        //window.open("{% url 'home' %}", '_self');
         location.replace("/loading/"+encoded_textValue, '_self');
-        //window.open("http://www.google.com/search?q="+textValue+"&source=lnms&tbm=isch",'_self');
 }
 goTo();
--- EB/searchWord/static/searchWord/js/passWordToS3.js
+++ EB/searchWord/static/searchWord/js/passWordToS3.js
@@ -1,8 +1,5 @@
 function passWordToS3(){
-	parsedQueryWord = document.currentScript.getAttribute('queryWordOnScript');
-        setTimeout(function(){
-                location.replace("/board/"+parsedQueryWord, '_self');
-        }, 15000);
+	parsedQueryWord = document.currentScript.getAttribute('queryWordOnScript')
+	setTimeout(function(){location.replace("/board/"+parsedQueryWord, '_self')}, 0)
 }
-
-passWordToS3();
+passWordToS3()
