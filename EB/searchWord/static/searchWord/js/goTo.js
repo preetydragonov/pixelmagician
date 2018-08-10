5af9aaff6592feb21512914e76db50698b66b859
preetydragonov <flsqja12@naver.com>
2018-08-10 13:16:45 +0900

    use text_box

--- EB/searchWord/static/searchWord/js/goTo.js
+++ EB/searchWord/static/searchWord/js/goTo.js
@@ -1,5 +1,5 @@
 function goTo(){
-	const textValue = document.getElementById("link_id").value;
+	const textValue = document.getElementById("text_box").value;
         const encoded_textValue = encodeURIComponent(textValue);
         location.replace("/loading/"+encoded_textValue, '_self');
 }
