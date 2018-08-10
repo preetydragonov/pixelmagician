6ff7cc60bcf3bce78e465b79dc7ce96ddfdd9eb0
preetydragonov <flsqja12@naver.com>
2018-08-10 16:12:51 +0900

    change double quotes to quote

--- EB/searchWord/static/searchWord/js/goTo.js
+++ EB/searchWord/static/searchWord/js/goTo.js
@@ -1,6 +1,6 @@
 function goTo(){
-	const textValue = document.getElementById("text_box").value;
+	const textValue = document.getElementById('text_box').value;
         const encoded_textValue = encodeURIComponent(textValue);
-        location.replace("/loading/"+encoded_textValue, '_self');
+        location.replace('/loading/'+encoded_textValue, '_self');
 }
 goTo();
