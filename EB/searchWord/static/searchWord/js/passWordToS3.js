9193a82f9a2637b4677b65187e5a494e5ad8e45a
preetydragonov <flsqja12@naver.com>
2018-08-16 14:49:12 +0900

    change setTimeOut to 10 sec

--- EB/searchWord/static/searchWord/js/passWordToS3.js
+++ EB/searchWord/static/searchWord/js/passWordToS3.js
@@ -1,5 +1,5 @@
 function passWordToS3(){
 	parsedQueryWord = document.currentScript.getAttribute('queryWordOnScript')
-	setTimeout(function(){location.replace("/board/"+parsedQueryWord, '_self')}, 0)
+	setTimeout(function(){location.replace("/board/"+parsedQueryWord, '_self')}, 7000)
 }
 passWordToS3()
