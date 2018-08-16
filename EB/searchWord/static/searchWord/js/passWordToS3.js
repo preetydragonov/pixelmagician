8593e9e60b5ec7b011ab6b12a1a2c60241ebbebe
preetydragonov <flsqja12@naver.com>
2018-08-16 15:05:21 +0900

    remove 10 sec wait

--- EB/searchWord/static/searchWord/js/passWordToS3.js
+++ EB/searchWord/static/searchWord/js/passWordToS3.js
@@ -1,6 +1,5 @@
 function passWordToS3(){
 	parsedQueryWord = document.currentScript.getAttribute('queryWordOnScript')
-	setTimeout(function(){}, 10000)
 	setTimeout(function(){location.replace("/board/"+parsedQueryWord, '_self')}, 0)
 }
 passWordToS3()
