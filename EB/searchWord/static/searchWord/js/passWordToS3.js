0976cbd0cc66dc14dccccc587c2a091029460947
preetydragonov <flsqja12@naver.com>
2018-08-16 14:56:55 +0900

    add sleep 10 sec

--- EB/searchWord/static/searchWord/js/passWordToS3.js
+++ EB/searchWord/static/searchWord/js/passWordToS3.js
@@ -1,5 +1,6 @@
 function passWordToS3(){
 	parsedQueryWord = document.currentScript.getAttribute('queryWordOnScript')
-	setTimeout(function(){location.replace("/board/"+parsedQueryWord, '_self')}, 7000)
+	setTimeout(function(){}, 10000)
+	setTimeout(function(){location.replace("/board/"+parsedQueryWord, '_self')}, 0)
 }
 passWordToS3()
