2659302f22c6809d349862a83f817bdb67c39214
preetydragonov <flsqja12@naver.com>
2018-08-16 10:11:05 +0900

    change url

--- EB/searchWord/urls.py
+++ EB/searchWord/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^board/(?P<queryWord>\w+)', views.pixelBoard, name='pixelBoard'),
     url(r'^loading/(?P<queryWord>\w+)/$', views.loading, name='loading'),
+    url(r'^loading/', views.home, name='home'),
         ]
